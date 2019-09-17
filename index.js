a2f664d9e75c2644d2362cb4bce31dc8dcff8f73
yiz132 <yiz132@pitt.edu>
2019-09-17 00:11:54 -0400

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 // express.watch for incoming HTTP requests. Watch for request trying to access '/'. Obj representing the incoming request. 
 // Res: obj repre the out response. immediately send some json back to request
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'buddy'});
 });
 
-app.listen(5000);
+//env: environment varible. if there isnt a port visit 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
